ad3231b8b0e1ed8ce4fd11d03f5c97441197271b
jlake70 <jlake7021@gmail.com>
2024-05-02 13:26:56 -0400

    Question 6 completed

---
+++ Exploring Logical Operations and Precedence.py
@@ -0,0 +1,14 @@
+# Question 6 Task 1
+# The aim of this assignment is to grasp the concept of 
+# logical operations and understand how operator precedence 
+# can affect the outcome of an operation.
+
+value_a = 6+7*3
+print(value_a)
+
+value_b = 6 + (7*3)
+print(value_b)
+
+#Question 6 Task 2
+result = 6+7 > 3 or 7+3 > 5
+print(result)
