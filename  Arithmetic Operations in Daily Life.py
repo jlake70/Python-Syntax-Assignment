89aaacdff23d6e6e4d4789efb9afda563d94c169
jlake70 <jlake7021@gmail.com>
2024-05-02 12:48:38 -0400

    Question 4 answered

---
+++  Arithmetic Operations in Daily Life.py
@@ -0,0 +1,14 @@
+# Question 4 Task 1
+peanutButter = 7
+jelly = 4
+bread = 5
+
+totalCost = peanutButter + jelly + bread
+print(totalCost)
+
+#Question 4 Task 2
+account_balance = 10,000
+INTEREST = 7/100
+
+account_balance_interest = 10000 *.07 + 10000
+print(account_balance_interest)
