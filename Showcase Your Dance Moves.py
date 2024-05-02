084454d30257c2ce2d9a7f6a56c6fdb3e965c159
jlake70 <jlake7021@gmail.com>
2024-05-02 12:06:23 -0400

    Python assignment question 1

---
+++ Showcase Your Dance Moves.py
@@ -0,0 +1,13 @@
+# Question 1 Task 1
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+# Question 1 Task 2
+feeling = "sad"
+
+if feeling == "sad":
+    print("I hope your day gets better!")
